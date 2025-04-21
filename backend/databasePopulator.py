dd5c865a6d40512d67f9379d5027a8547142aa14
parikhro <100094364+parikhro@users.noreply.github.com>
2025-04-20 22:45:33 -0400

    database update

--- backend/databasePopulator.py
+++ backend/databasePopulator.py
@@ -18,70 +18,94 @@ hashMapProf = defaultdict(dict)
 hashMapProfCourse = defaultdict(dict)
 hashMapCourse = defaultdict(dict)
 
+def commaNameToName(name: str) -> str:
+    name = name.split(",")
+    fullName = name[0]
+    if len(name) == 2:
+        fullName = (name[1] + " " + fullName)[1:]
+    return fullName
 
 
-for file_path in glob.glob(f'backend/rmp-scraper/Summarized_Reviews/*.csv'):
-    if 'cut.csv' not in file_path:
-        continue
+# LOAD SUMMARY
+for file_path in glob.glob(f'backend/rmp-scraper/summarized/*.csv'):
     with open(file_path, 'r', encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file)
-        next(reader)
-
-        for row in reader:  
-            hashMapProf[row[0]].update({'summary': row[1]})
-    
-
-
-
-for file_path in glob.glob(f'backend/sentiment_analysis/Ratings Data/*.csv'):
-    #print("HEHEHEHEHEHEHEHEHEEHEHEHEHEE")
-    with open(file_path, 'r', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)
-    
-        for row in reader:
-            hashMapProf[row[0]].update({'rating': float(row[1])})
-
-"""for file_path in glob.glob(r'*backend/class_and_pre-recs/*.csv'):
-    with open(file_path, 'r', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)
+        # next(reader) # there is no header here
 
         for row in reader:
-            hashMapCourse[row[0]].update({'summary': row[1], 'rating': float(row[2])})"""
+            name = row[0]
+            summary = row[1]
+            hashMapProf[name].update({'summary': summary})
+    
+print(f"Loaded {len(hashMapProf)} professors with summaries; sample:\n",
+      list(hashMapProf.items())[:5])
 
 
+# LOAD SENTIMENT
+for file_path in glob.glob(f'backend/sentiment_analysis/Updated Ratings Data/*.csv'):
+    print("HEHEHEHEHEHEHEHEHEEHEHEHEHEE")
+    with open(file_path, 'r', encoding='utf-8') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader) # skip header
 
+        for row in reader:
+            name = row[0]
+            rating = row[1]
+            hashMapProf[name].update({'rating': float(rating)})
+
+# LOAD SQI
 courses = ['Chemistry', 'Physics', 'Math', 'Computer Science', 'Engineering']
-
 for course in courses:
     for file_path in glob.glob(f'backend/Grade-ient_SQI/courses/{course}/*.csv'):
-        #print(file_path)
-
         with open(file_path, 'r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
-            #next(reader)
-            
-            if 'professor_SQI.csv' in file_path and 'course-professor_SQI.csv' not in file_path:
-                    
+
+            if '_professor_SQI.csv' in file_path:
                 for row in reader:
-                    hashMapProf[row[0]].update({'SQI': float(row[1])})
+                    name = row[0]
+                    name = commaNameToName(name)
+                    sqi = row[1]
+                    hashMapProf[name].update({'SQI': float(sqi)})
 
             elif 'course_SQI.csv' in file_path:     
                 #print('in here')
                 for row in reader:
-                    hashMapCourse[row[0]].update({'SQI': float(row[2]), 'course_name': row[1]})
+                    courseCode = row[0]
+                    courseName = row[1]
+                    sqi = row[2]
+                    hashMapCourse[courseCode].update({'SQI': float(sqi), 'course_name': courseName})
 print(hashMapCourse)
-
+print(f"len of courses: {len(hashMapCourse)}")
 print(hashMapProf)
+print(f"len of profs: {len(hashMapProf)}")
 
-print(hashMapProf.items())
+# print(hashMapProf.items())
+
 try:
     with conn.cursor() as cur:
-        execute_batch(cur, """
-            INSERT INTO Professor (prof_name, netid, summary, metrics, SQI)
-            VALUES (%s, %s, %s, %s, %s) ON CONFLICT (prof_name) DO NOTHING""", 
-            [(name, '', data.get('summary'), data.get('rating'), data.get('SQI')) for name, data in hashMapProf.items()])
+        batch_values = []
+        for name, data in hashMapProf.items():
+            rating = data.get('rating')
+            sqi_raw = data.get('SQI')
+            
+            combined_sqi = sqi_raw if rating is None else round((sqi_raw + rating) / 2,2)
+
+            batch_values.append((
+                name,
+                data.get('summary'),
+                combined_sqi
+            ))
+        print("Will insert", len(batch_values), "professors")
+        execute_batch(
+            cur,
+            """
+            INSERT INTO Professor (prof_name, summary, SQI)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (prof_name) DO NOTHING
+            """,
+            batch_values
+        )
+    conn.commit()
 except Exception as e:
     print(f"Error inserting into Professor table: {e}")
     
@@ -97,15 +121,15 @@ for course in courses:
         with open(file_path, 'r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
             next(reader)
-
             for row in reader:
-
-                #try:
-
-                
+                # try:
                     if 'course-professor_SQI.csv' in file_path:
                         #print(row)
-
+                        profName = row[2]
+                        profName = commaNameToName(profName)
+                        courseCode = row[0]
+                        courseName = row[1]
+                        sqi = row[3]
                         with conn.cursor() as cur:
                             execute_batch(cur, """INSERT INTO Teaches (prof_id,class_id, SQI) VALUES (
                                         
@@ -113,32 +137,31 @@ for course in courses:
                                         (SELECT id FROM Class WHERE course_code = %s),
                                           %s
                                         
-                                        ) ON CONFLICT (prof_id,class_id) DO NOTHING """,[(row[2],row[0],row[3])])
-                #except Exception as e:
-                #   print("encountered something that doesn't exist")
-                 #   continue
+                                        ) ON CONFLICT (prof_id,class_id) DO NOTHING """,[(profName,courseCode,sqi)])
+                # except Exception as e:
+                #     print("encountered something that doesn't exist")
+                #     continue
 
-for file_path in glob.glob(f'backend/class_and_pre-rec_scraper/data/course_prereqs/*.csv'):
-    print('IN HERERERE')
-    with open(file_path, 'r', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader)
+# for file_path in glob.glob(f'backend/class_and_pre-rec_scraper/data/course_prereqs/*.csv'):
+#     with open(file_path, 'r', encoding='utf-8') as csv_file:
+#         reader = csv.reader(csv_file)
+#         next(reader)
 
-        for row in reader:
-           # print(row)
-            with conn.cursor() as cur:
+#         for row in reader:
+#            # print(row)
+#             with conn.cursor() as cur:
 
-                cur.execute("SELECT id FROM Class WHERE course_code ILIKE %s", (f'%:{row[0]}',))
-                class_id = cur.fetchone()
+#                 cur.execute("SELECT id FROM Class WHERE course_code ILIKE %s", (f'%:{row[0]}',))
+#                 class_id = cur.fetchone()
 
-                cur.execute("SELECT id FROM Class WHERE course_code ILIKE %s", (f'%:{row[2]}',))
-                pre_req_id = cur.fetchone()
+#                 cur.execute("SELECT id FROM Class WHERE course_code ILIKE %s", (f'%:{row[2]}',))
+#                 pre_req_id = cur.fetchone()
 
-                if class_id and pre_req_id:
-                    cur.execute("INSERT INTO Pre_Reqs (class_id, pre_req_group, pre_req_id) VALUES (%s, %s, %s) ON CONFLICT (class_id, pre_req_id) DO NOTHING",
-                               (class_id[0], row[1], pre_req_id[0]))
-                else:
-                    print(f"Class or pre-requisite not found for {row[0]} or {row[2]}")
+#                 if class_id and pre_req_id:
+#                     cur.execute("INSERT INTO Pre_Reqs (class_id, pre_req_group, pre_req_id) VALUES (%s, %s, %s) ON CONFLICT (class_id, pre_req_id) DO NOTHING",
+#                                (class_id[0], row[1], pre_req_id[0]))
+#                 else:
+#                     print(f"Class or pre-requisite not found for {row[0]} or {row[2]}")
 
 print("Connected to PostgreSQL!")
 conn.commit()
--- backend/schema.sql
+++ backend/schema.sql
@@ -5,9 +5,9 @@ CREATE TABLE Class(
     course_code VARCHAR(10) UNIQUE NOT NULL,
     course_name VARCHAR(100) NOT NULL,
     --pre_req INT references Class(id) ON DELETE SET NULL,
-    co_req INT references Class(id) ON DELETE SET NULL,
-    SQI DOUBLE PRECISION,
-    pre_req_group INT
+    -- co_req INT references Class(id) ON DELETE SET NULL,
+    SQI DOUBLE PRECISION
+    -- pre_req_group INT
 
 );
 
@@ -31,9 +31,9 @@ CREATE TABLE Professor(
 
     id SERIAL PRIMARY KEY,
     prof_name VARCHAR(100) UNIQUE,-- NOT NULL,
-    netid VARCHAR(8), --UNIQUE, --NOT NULL
+    -- netid VARCHAR(8), --UNIQUE, --NOT NULL
     --need more info for this
-    metrics DOUBLE PRECISION,
+    -- metrics DOUBLE PRECISION,
     summary TEXT,
     SQI DOUBLE PRECISION
 
